90f76fd8bc83c769d7e03c446e98c2f92de886cb
Abdul Hadi <abdulhadi03002270690@gmail.com>
2024-02-13 01:42:51 +0500

    Eraser_Icon made

---
+++ src/components/Eraser_Icon.tsx
@@ -0,0 +1,18 @@
+// icon:eraser-variant | Material Design Icons https://materialdesignicons.com/ | Austin Andrews
+import * as React from "react";
+
+function IconEraserVariant(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      height="1em"
+      width="1em"
+      {...props}
+    >
+      <path d="M15.14 3c-.51 0-1.02.2-1.41.59L2.59 14.73c-.78.77-.78 2.04 0 2.83L5.03 20h7.66l8.72-8.73c.79-.77.79-2.04 0-2.83l-4.85-4.85c-.39-.39-.91-.59-1.42-.59M17 18l-2 2h7v-2" />
+    </svg>
+  );
+}
+
+export default IconEraserVariant;
