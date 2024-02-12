af8cd32cd013c4c687987f93e9f47e88f3cc8bdd
Muhammad Huzaifa <mhuzaifa134@gmail.com>
2024-02-12 19:01:18 +0500

    dropdown

--- src/components/DropDown.jsx
+++ src/components/DropDown.jsx
@@ -5,7 +5,7 @@ import FormHelperText from '@mui/material/FormHelperText';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 
-export default function SelectLabels() {
+export default function SelectLabels({placeholder}) {
   const [age, setAge] = React.useState('');
 
   const handleChange = (event) => {
@@ -14,39 +14,22 @@ export default function SelectLabels() {
 
   return (
     <div>
-      <FormControl sx={{ m: 1, minWidth: 120 }}>
-        <InputLabel id="demo-simple-select-helper-label">Age</InputLabel>
-        <Select
-          labelId="demo-simple-select-helper-label"
-          id="demo-simple-select-helper"
-          value={age}
-          label="Age"
-          onChange={handleChange}
-        >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
-        <FormHelperText>With label + helper text</FormHelperText>
-      </FormControl>
-      <FormControl sx={{ m: 1, minWidth: 120 }}>
+      
+      <FormControl sx={{ m: 1, minWidth: 720 }}>
         <Select
           value={age}
           onChange={handleChange}
           displayEmpty
           inputProps={{ 'aria-label': 'Without label' }}
+          className='bg-white'
         >
           <MenuItem value="">
-            <em>None</em>
+            <p className='text-gray-400'>{placeholder}</p>
           </MenuItem>
           <MenuItem value={10}>Ten</MenuItem>
           <MenuItem value={20}>Twenty</MenuItem>
           <MenuItem value={30}>Thirty</MenuItem>
         </Select>
-        <FormHelperText>Without label</FormHelperText>
       </FormControl>
     </div>
   );
