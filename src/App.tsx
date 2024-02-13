2819fda564ce7c6eb54c6cd9c244f1a34b9637e9
Muhammad Huzaifa <mhuzaifa134@gmail.com>
2024-02-13 15:54:20 +0500

    ty

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "@polkadot-cloud/core/accent/polkadot-relay.css";
+import "@polkadot-cloud/core/theme/cloud/index.css";
+import Page from "./page/Page.component.tsx";
+import React from "react";
+
+function App() {
+  return (
+    <div className="theme-polkadot-relay theme-light ">
+      <div className="App">
+        <Page />
+      </div>
+    </div>
+  );
+}
+
+export default App;
